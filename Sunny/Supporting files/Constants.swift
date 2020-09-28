7de30d5ee5efd1beeebf7fd4ffd853567ffa5cec
Nikita Kochetov <nothsmple@mail.ru>
2020-09-28 20:55:14 +0300

    Final

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Татьяна Кочетова on 23.09.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "48fd19ea0bda09ae97635627bc916dcd"
